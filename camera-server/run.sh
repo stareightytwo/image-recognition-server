57a356b18a45043516e74a57c57b38c9db007418
StarEightyTwo <45858547+stareightytwo@users.noreply.github.com>
2019-01-04 12:02:23 -0600

    Create venv if doesn't exist already -- completes camera server runner

--- camera-server/run.sh
+++ camera-server/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if ! [[ -d venv ]]; then
+    cd setup && ./create_venv.sh && cd ..
+fi
+
 source setup/venv/bin/activate
 
 gunicorn --reload --config server/configs/gunicorn_config.py "server.app:create_app()"
