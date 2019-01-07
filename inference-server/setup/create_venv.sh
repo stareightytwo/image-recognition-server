92854710d299ecb3e3200268261a932bcf4d4283
jake-mason <jcmason24@gmail.com>
2019-01-07 08:54:54 -0600

    Tensorflow not supported for Python 3.7

--- inference-server/setup/create_venv.sh
+++ inference-server/setup/create_venv.sh
@@ -4,21 +4,20 @@ if [[ -d venv ]]; then
     rm -rf venv/
 fi
 
-virtualenv venv
+virtualenv venv --python /usr/local/bin/python3
+
 source venv/bin/activate
 
 pip3 install --upgrade pip
 pip3 install -r ../requirements.txt
 
-if ! [[ -d darkflow ]]; then
-	git clone https://github.com/thtrieu/darkflow.git
-	cd darkflow
+git clone https://github.com/thtrieu/darkflow.git
+cd darkflow
 
-	pip3 install .
+pip3 install .
 
-	git clone https://github.com/leetenki/YOLOtiny_v2_chainer.git
-	mkdir bin
-	mv YOLOtiny_v2_chainer/tiny-yolo-voc.weights bin/
+git clone https://github.com/leetenki/YOLOtiny_v2_chainer.git
+mkdir bin
+mv YOLOtiny_v2_chainer/tiny-yolo-voc.weights bin/
 
-	rm -rf YOLOtiny_v2_chainer
-fi
+rm -rf YOLOtiny_v2_chainer
